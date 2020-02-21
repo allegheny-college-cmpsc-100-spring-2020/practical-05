1a588f6e63d0809b94c6d2e30f021fc39c9eaa66
Douglas Luman <dluman@douglasjluman.com>
2020-02-21 10:43:10 -0500

    Initial commit

---
+++ src/main/java/practicalfive/RunForIt.java
@@ -0,0 +1,21 @@
+package practicalfive;
+
+/** Creates dice to play "Run For It".
+ *
+ * @author Solutions Repository
+ */
+public class RunForIt {
+  
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO: Initialize a Die object of six sides.
+    /* 
+     * TODO: Implement the roll method of Die to generate
+     *       a six-sided die result six times and print 
+     *       the results in a single line separated by a space
+     */
+  }
+}
